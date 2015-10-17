22c3bc0fb7b4ae82ce472474d3f398cf777c8066
Forb.Jok <forbjok@gmail.com>
2015-10-17 19:20:49 +0200

    Greentext support

---
+++ src/valueconverters/postformat.js
@@ -0,0 +1,9 @@
+export class PostFormatValueConverter {
+  toView(value) {
+    if (!value)
+      return value;
+
+    return value.replace(/((^|\n)\>.*)/g, '<span class="quote">$1</span>')
+      .replace(/\n/g, '<br>');
+  }
+}
