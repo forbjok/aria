f2aea97e05adc7f06472987ab70e33976dcc5b04
Forb.Jok <forbjok@gmail.com>
2015-12-12 00:11:10 +0100

    feat(chat): Make links clickable in posts

--- src/frontend/valueconverters/formatpost.js
+++ src/frontend/valueconverters/formatpost.js
@@ -5,7 +5,9 @@ export class FormatPostValueConverter {
     }
 
     /*eslint quotes: 0*/
-    return value.replace(/((^|\n)\>.*)/g, '<span class="quote">$1</span>')
-      .replace(/\n/g, '<br>');
+    return value
+      .replace(/((^|\n)\>.*)/g, '<span class="quote">$1</span>') // Color quotes
+      .replace(/(https?:\/\/[^\s]+)/g, '<a href="$1">$1</a>') // Clickable links
+      .replace(/\n/g, '<br>'); // Convert newlines to HTML line breaks
   }
 }
