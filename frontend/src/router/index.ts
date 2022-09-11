f0e9b7e535cff9555a9519df861e0a742eae6dc4
Forb.Jok <forbjok@gmail.com>
2022-09-11 19:43:50 +0200

    fix: Vue warning in console

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,7 @@
-import { defineAsyncComponent } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 
-const ClaimView = defineAsyncComponent(() => import("@/views/ClaimView.vue"));
-const RoomView = defineAsyncComponent(() => import("@/views/RoomView.vue"));
+const ClaimView = () => import("@/views/ClaimView.vue");
+const RoomView = () => import("@/views/RoomView.vue");
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
