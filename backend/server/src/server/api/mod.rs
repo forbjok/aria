eb056f81fae9c9427f828c63cf011b6a91eebfbd
Forb.Jok <forbjok@gmail.com>
2022-09-18 21:39:28 +0200

    feat(backend): Return error message in response body on internal server error

--- backend/server/src/server/api/mod.rs
+++ backend/server/src/server/api/mod.rs
@@ -47,7 +47,7 @@ impl IntoResponse for ApiError {
         match self {
             Self::Anyhow(err) => {
                 error!("{err:#}");
-                (StatusCode::INTERNAL_SERVER_ERROR, ()).into_response()
+                (StatusCode::INTERNAL_SERVER_ERROR, format!("{err:#}")).into_response()
             }
             Self::BadRequest => (StatusCode::BAD_REQUEST, ()).into_response(),
             Self::NotFound => (StatusCode::NOT_FOUND, ()).into_response(),
