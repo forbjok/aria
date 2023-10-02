3f81232b14f1f125aabb11b4e3ff6d7f861b41c9
Forb.Jok <forbjok@gmail.com>
2023-10-02 23:59:51 +0200

    feat: Don't show application until initialization has run, and show error message if it fails

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,50 @@
 <script setup lang="ts">
-import { onBeforeMount } from "vue";
+import { onBeforeMount, ref } from "vue";
 import { RouterView } from "vue-router";
 
 import { useMainStore } from "@/stores/main";
 
+const isInitialized = ref(false);
+const isLoading = ref(true);
+const errorMessage = ref<string>();
+
 const mainStore = useMainStore();
 
 onBeforeMount(async () => {
-  await mainStore.initialize();
+  try {
+    await mainStore.initialize();
+
+    isInitialized.value = true;
+  } catch (err) {
+    errorMessage.value = err as string;
+  }
+
+  isLoading.value = false;
 });
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-if="isInitialized" />
+  <div v-if="!isInitialized" class="loading">
+    <div v-show="isLoading" class="loading-text">LOADING</div>
+    <div v-show="errorMessage" class="error-text">{{ errorMessage }}</div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.loading {
+  font-size: 1.4rem;
+
+  padding: 2rem;
+
+  text-align: center;
+
+  .loading-text {
+    color: white;
+  }
+
+  .error-text {
+    color: red;
+  }
+}
+</style>
